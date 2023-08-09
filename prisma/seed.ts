93876c5438f92bf41aa7a869603fa4fed093efcf
luizfiorentino <lgfiorentino@gmail.com>
2023-08-09 17:58:28 +0200

    seeds.ts file created in the prisma folder with a script to createMany entries in the Company model table. DB up to date with seeded data.

---
+++ prisma/seed.ts
@@ -0,0 +1,50 @@
+const companiesWithCuid = require("../dataCleaning/companiesCuid.json");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    // Seed data using createMany
+    const seededData = await prisma.company.createMany({
+      data: companiesWithCuid.map((company: any) => {
+        const {
+          id,
+          name,
+          referentNumber,
+          webpageUrl,
+          category,
+          city,
+          street,
+          houseNumber,
+          postCode,
+        } = company;
+
+        return {
+          id,
+          name,
+          indReferentNumber: referentNumber,
+          website: webpageUrl,
+          category,
+          city,
+          street,
+          houseNumber,
+          postCode,
+        };
+        // ...company,
+        // indReferentNumber: company.referentNumber,
+        // website: company.webpageUrl,
+        // id: company.id,
+      }),
+      skipDuplicates: true, // skip duplicates
+    });
+
+    console.log(seededData, "Seeded Data:");
+  } catch (error) {
+    console.error("Error seeding data:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
