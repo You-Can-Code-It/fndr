b5588b3b935ad1525baf8634131d73884ad680f3
luizfiorentino <lgfiorentino@gmail.com>
2023-08-23 15:57:45 +0200

    AddCompanyForm styled first step.

--- components/AddCompanyForm/AddCompanyForm.tsx
+++ components/AddCompanyForm/AddCompanyForm.tsx
@@ -4,7 +4,8 @@ import { z } from "zod";
 import axios from "axios";
 import styles from "./AddCompanyForm.module.css";
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import Logo from "../logo/Logo";
+import Heading1 from "../typography/Heading1";
 const formSchema = z.object({
   name: z
     .string()
@@ -72,81 +73,100 @@ const AddCompanyForm: React.FC = () => {
 
   return (
     <div className={styles.addCompanyForm}>
-      <form onSubmit={handleSubmit(formSubmit)}>
-        <input
-          type="text"
-          placeholder="Company Name"
-          {...register("name")}
-          className={styles.addCompanyForm}
-        />
-        {errors.name && <p className="error">{String(errors.name.message)}</p>}
-        <input
-          type="text"
-          placeholder="Ind Number"
-          {...register("indReferentNumber")}
-          className={styles.addCompanyForm}
-        />
-        {errors.indReferentNumber && (
-          <p className="error">{String(errors.indReferentNumber.message)}</p>
-        )}
-        <input
-          type="text"
-          placeholder="Website"
-          {...register("website")}
-          className={styles.addCompanyForm}
-        />
-        {errors.website && (
-          <p className="error">{String(errors.website.message)}</p>
-        )}
-        <input
-          type="text"
-          placeholder="Category"
-          {...register("category")}
-          className={styles.addCompanyForm}
-        />
-        {errors.category && (
-          <p className="error">{String(errors.category.message)}</p>
-        )}
-        <input
-          type="text"
-          placeholder="City"
-          {...register("city")}
-          className={styles.addCompanyForm}
-        />
-        {errors.city && <p className="error">{String(errors.city.message)}</p>}
-        <input
-          type="text"
-          placeholder="Street"
-          {...register("street")}
-          className={styles.addCompanyForm}
-        />
-        {errors.street && (
-          <p className="error">{String(errors.street.message)}</p>
-        )}
-        <input
-          type="text"
-          placeholder="Number"
-          {...register("houseNumber")}
-          className={styles.addCompanyForm}
-        />
-        {errors.houseNumber && (
-          <p className="error">{String(errors.houseNumber.message)}</p>
-        )}
-        <input
-          type="text"
-          placeholder="Postal code"
-          {...register("postCode")}
-          className={styles.addCompanyForm}
-        />
-        {errors.postCode && (
-          <p className="error">{String(errors.postCode.message)}</p>
-        )}
+      <div className={styles.logo}>
+        <Logo />
+      </div>
 
-        <div className={styles.addCompanyForm}>
-          {" "}
-          <button type="submit">Add Company</button>
+      <div className={styles.formInputs}>
+        <h3 className={styles.formBanner}>New Company</h3>
+        <div className={styles.forInputsInner}>
+          <form onSubmit={handleSubmit(formSubmit)}>
+            <label className={styles.formLabel}>Name</label>
+            <input
+              type="text"
+              placeholder="Company Name"
+              {...register("name")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.name && (
+              <p className="error">{String(errors.name.message)}</p>
+            )}
+            <label className={styles.formLabel}>IND Number</label>
+            <input
+              type="text"
+              placeholder="Ind Number"
+              {...register("indReferentNumber")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.indReferentNumber && (
+              <p className="error">
+                {String(errors.indReferentNumber.message)}
+              </p>
+            )}
+            <label className={styles.formLabel}>Website</label>
+            <input
+              type="text"
+              placeholder="Website"
+              {...register("website")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.website && (
+              <p className="error">{String(errors.website.message)}</p>
+            )}
+            <label className={styles.formLabel}>Category</label>
+            <input
+              type="text"
+              placeholder="Category"
+              {...register("category")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.category && (
+              <p className="error">{String(errors.category.message)}</p>
+            )}
+            <label className={styles.formLabel}>City</label>
+            <input
+              type="text"
+              placeholder="City"
+              {...register("city")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.city && (
+              <p className="error">{String(errors.city.message)}</p>
+            )}
+            <label className={styles.formLabel}>Street</label>
+            <input
+              type="text"
+              placeholder="Street"
+              {...register("street")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.street && (
+              <p className="error">{String(errors.street.message)}</p>
+            )}
+            <label className={styles.formLabel}>Number</label>
+            <input
+              type="text"
+              placeholder="Number"
+              {...register("houseNumber")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.houseNumber && (
+              <p className="error">{String(errors.houseNumber.message)}</p>
+            )}
+            <label className={styles.formLabel}>Zip Code</label>
+            <input
+              type="text"
+              placeholder="Postal code"
+              {...register("postCode")}
+              className={styles.addCompanyFormInput}
+            />
+            {errors.postCode && (
+              <p className="error">{String(errors.postCode.message)}</p>
+            )}{" "}
+            <button type="submit">Add Company</button>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
