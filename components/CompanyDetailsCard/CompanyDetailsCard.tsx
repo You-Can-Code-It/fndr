46018ee5fc109da0428abaa6bfdd4c1e7e82eb87
luizfiorentino <lgfiorentino@gmail.com>
2023-08-23 11:01:56 +0200

    CSS module file created to style CompanyDetailsCard component.

--- components/CompanyDetailsCard/CompanyDetailsCard.tsx
+++ components/CompanyDetailsCard/CompanyDetailsCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./CompanyDetailsCard.module.css";
 
 type Company = {
   id: string;
@@ -20,14 +21,14 @@ type CompanyDetailsCardProps = {
 
 const CompanyDetailsCard: React.FC<CompanyDetailsCardProps> = ({ company }) => {
   return (
-    <div>
+    <div className={styles.detailsCardMainContainer}>
       <h2>{company?.name}</h2>
       <h4>City: {company?.city}</h4> <h4>Activity: {company?.category}</h4>
       <h4> {company?.website}</h4>
       <h5>IND number: {company?.indReferentNumber}</h5>
       <h5>
         Address:{" "}
-        {`${company?.street}, ${company.houseNumber}. ${company?.city}. ${company?.postCode}`}
+        {`${company?.street}, ${company?.houseNumber}. ${company?.city}. ${company?.postCode}`}
       </h5>
     </div>
   );
