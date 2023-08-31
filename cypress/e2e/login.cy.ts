88caa48dbe9712ddda44a0b59e1d70fb03798f0e
Reinoptland <reinoptland@gmail.com>
2023-08-31 07:32:53 +0200

    Cleanup login test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,8 +1,4 @@
-describe("template spec", () => {
-  after(() => {
-    cy.exec("npm run testdb:reseed");
-  });
-
+describe("login", () => {
   it("should display the avatar image in the navbar after logging in", () => {
     cy.visit("localhost:3000");
     cy.get('img[alt="User avatar"]').should("not.exist");
