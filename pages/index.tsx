5f217ddfea51c212545a6f4a8b8cdb0dd10eaf30
luizfiorentino <lgfiorentino@gmail.com>
2023-09-13 13:17:00 +0200

    Styling: displaying two columns of <Card /> component in bigger screen versions.

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,7 +168,7 @@ function Home({
 
         <div className={styles.companiesCards}>
           {/* Needs fix: For design issues, displaying only the first 84 results. */}
-          {response.companies.slice(0, 84).map((company: Company, index) => {
+          {response.companies.map((company: Company) => {
             return (
               <Card
                 key={company.id}
